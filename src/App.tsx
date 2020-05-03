721db06183bf7745c31a217a56d673efaa90992d
Ethan Miller <ethdmill@gmail.com>
2020-05-03 17:07:51 -0400

    added react and typescript

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+import './App.css';
+import mainPage from './pages/main'
+
+function App() {
+  return (
+    mainPage()
+  );
+}
+
+export default App;
---
+++ src/pages/main.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default function mainPage () {
+    return (
+        <div>
+            this is a big ol' test div
+        </div>
+    )
+}
